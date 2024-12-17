ebb7e453ff3685c4dc627c57c5c747e3a4a44876
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 06:57:25 +0800

    feat: add forms

---
+++ pemweb_django_crud/todolist/forms.py
@@ -0,0 +1,27 @@
+from django import forms 
+from django.forms import ModelForm
+from . import models
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+  model = models.Task
+  # fields = ['title', 'completed', 'due_date']
+  # widgets = {
+  #           'title': forms.TextInput(attrs={
+  #               'class': 'form-control',
+  #               'placeholder': 'Add in a new task'
+  #           }),
+  #           'completed' : forms.BooleanField(attrs={
+  #             'class' : 'form-control'
+  #           })
+  #           ,
+  #           'due_date': forms.DateTimeInput(attrs={
+  #               'class': 'form-control',
+  #               'type': 'datetime-local'
+  #           }),
+  #       }
+  
+  title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add in a new task'}))
+  class Meta:
+    model = Task
+    fields = "__all__"
