6ae4ee7ee3f1e7edaf6bc0c67b33b443282072e4
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 07:04:04 +0800

    feat: load model in html

--- pemweb_django_crud/todolist/views.py
+++ pemweb_django_crud/todolist/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
+  form = TaskForm()
   tasks = Task.objects.all()
-  context = {'tasks': tasks}
+  context = {'tasks': tasks, 'TaskForm':form}
   return render(request, 'todo.html', context)
