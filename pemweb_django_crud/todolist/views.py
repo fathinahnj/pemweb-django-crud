0c11f4d501c7f6b0428b428be0c1a8dc3419944e
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 07:11:16 +0800

    feat: add input task logic

--- pemweb_django_crud/todolist/views.py
+++ pemweb_django_crud/todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -6,5 +6,11 @@ from .forms import TaskForm
 def index(request):
   form = TaskForm()
   tasks = Task.objects.all()
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid:
+      form.save()
+    return redirect('/')
+  
   context = {'tasks': tasks, 'TaskForm':form}
   return render(request, 'todo.html', context)
