5748cf515f5e9fee85e588fe5971eeb2214787ef
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 11:39:41 +0800

    feat: add category table in todolist/models.py

--- pemweb_django_crud/todolist/forms.py
+++ pemweb_django_crud/todolist/forms.py
@@ -3,21 +3,6 @@ from django.forms import ModelForm
 from . import models
 from .models import Task
 
-  # fields = ['title', 'completed', 'due_date']
-  # widgets = {
-  #           'title': forms.TextInput(attrs={
-  #               'class': 'form-control',
-  #               'placeholder': 'Add in a new task'
-  #           }),
-  #           'completed' : forms.BooleanField(attrs={
-  #             'class' : 'form-control'
-  #           })
-  #           ,
-  #           'due_date': forms.DateTimeInput(attrs={
-  #               'class': 'form-control',
-  #               'type': 'datetime-local'
-  #           }),
-  #       }
 class TaskForm(forms.ModelForm):
 
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add in a new task'}))
