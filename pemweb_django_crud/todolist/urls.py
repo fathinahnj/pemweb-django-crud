820dbc089521eb73608357ae96089a4fcbe71a98
unknown <andiaisyaaah@gmail.com>
2024-12-18 19:04:45 +0800

    fe nya todo dkk

--- pemweb_django_crud/todolist/urls.py
+++ pemweb_django_crud/todolist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=""),
+    path('', views.index, name='todo-list'),
     path('update-task/<str:pk>', views.updateTask, name="update-task"),
     path('delete-task/<str:pk>', views.deleteTask, name="delete-task"),
     path('update-category/<int:pk>/', views.TaskForm, name='update-category'),
--- pemweb_django_crud/todolist/views.py
+++ pemweb_django_crud/todolist/views.py
@@ -1,34 +1,37 @@
-from django.shortcuts import render, redirect # type: ignore
-from django.http import HttpResponse # type: ignore
-from . models import Task, Category
-from . forms import TaskForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse  # type: ignore
+from .models import Task, Category
+from .forms import TaskForm
 
 def index(request):
-  form = TaskForm()
-  tasks = Task.objects.all()
-  categories = Category.objects.all()  # Fetch all categories
-  
-  if request.method == 'POST':
-    form = TaskForm(request.POST)
-    if form.is_valid:
-      form.save()
-      return redirect('/')
-    else:
-      print(form.errors) 
-  
-  context = {'tasks': tasks, 'TaskForm':form, 'categories': categories}
-  return render(request, 'todo.html', context)
+    form = TaskForm() 
+    tasks = Task.objects.all()
+    categories = Category.objects.all()  # Fetch all categories
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo-list')  # Redirect to 'todo-list' using its name
+
+    context = {'tasks': tasks, 'TaskForm': form, 'categories': categories}
+    return render(request, 'todo.html', context)
 
 def updateTask(request, pk):
-  task = Task.objects.get(id=pk)
-  form = TaskForm(instance=task)
-  if request.method == 'POST':
-    form = TaskForm(request.POST, instance=task)
-    if form.is_valid:
-      form.save()
-    return redirect('/')
-  context = {'TaskForm':form}
-  return render(request, 'update-task.html', context)
+    task = get_object_or_404(Task, id=pk)
+    categories = Category.objects.all()  # Fetch categories if needed in the template
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('todo-list')  # Redirect to homepage after saving
+    else:
+        form = TaskForm(instance=task)
+
+    context = {'form': form, 'task': task, 'categories': categories}
+    return render(request, 'update-task.html', context)
+
 
 def deleteTask(request, pk):
   task = Task.objects.get(id=pk)
