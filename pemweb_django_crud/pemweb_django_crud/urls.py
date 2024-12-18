8b12c8ea870e4bb8b4daf980b9c0241b066d01ab
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 10:25:50 +0800

    fix: fix category path

--- pemweb_django_crud/pemweb_django_crud/urls.py
+++ pemweb_django_crud/pemweb_django_crud/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todolist.urls')), 
+    path('category/', include('category.urls')), 
+
 ]
 
