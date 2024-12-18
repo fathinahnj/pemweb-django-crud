e68c0c501b224ed613980ee958ff6a1c1637fb20
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 09:47:17 +0800

    feat: add feature to delete task

--- pemweb_django_crud/todolist/views.py
+++ pemweb_django_crud/todolist/views.py
@@ -25,3 +25,12 @@ def updateTask(request, pk):
     return redirect('/')
   context = {'TaskForm':form}
   return render(request, 'update-task.html', context)
+
+def deleteTask(request, pk):
+  task = Task.objects.get(id=pk)
+  if request.method == 'POST':
+    task.delete()
+    return redirect('/')
+  context = {'task': task}
+  return render(request, 'delete-task.html', context)
+  
