71385ba68c8363a6a269ae38984dd20b7335778a
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-18 16:01:16 +0800

    fix integration bugs

--- pemweb_django_crud/todolist/models.py
+++ pemweb_django_crud/todolist/models.py
@@ -1,17 +1,16 @@
-from django.db import models # type: ignore
-
-class Category(models.Model):
-  title = models.CharField(max_length=30)
+from django.db import models
+from category.models import Category
+# class Category(models.Model):
+#   title = models.CharField(max_length=30)
   
-  def __str__(self):
-    return self.title 
+#   def _str_(self):
+#     return self.title 
 
 class Task(models.Model):
   title = models.CharField(max_length=300)
   completed = models.BooleanField(default=False)
   # due_date = models.DateTimeField(null=True, blank=True)
-  category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="tasks", default=1, null=True, blank=True) 
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True) 
   
-  def __str__(self):
-    return self.title 
-  
+  # def _str_(self):
+  #   return self.id
